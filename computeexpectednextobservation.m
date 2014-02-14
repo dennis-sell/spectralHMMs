52f2aa1a0674665fb5a25112d4e1ac406e0bb4f7
Dennis Sell <sell.dennis@gmail.com>
2014-02-14 18:36:02 -0500

    Implemented prediction of next observation given previous.

---
+++ computeexpectednextobservation.m
@@ -0,0 +1,23 @@
+function [prob] = computeexpectednextobservation( observationArray, spectralModel, nObservations)
+% Given a model and the series of observations seen so far, will calculate the
+% probabilities of seeing each possible observation next.
+
+T = size(observationArray,2);
+b_inf = spectralModel.b_inf;
+
+b_t = spectralModel.b1;
+for t=2:(T+1)
+   observation = observationArray(t-1);
+   B_x = spectralModel.B(:,:,observation);
+   b_t = B_x * b_t / (b_inf' * B_x * b_t);
+end
+
+prob = zeros(1,nObservations);
+for iObs = 1:nObservations
+    prob(iObs) = b_inf' * spectralModel.B(:,:,iObs) * b_t;
+end
+
+prob = prob ./ sum(prob);
+
+end
+
